ac5347c55fb67de111ae8aa80d3f35e360b8d8ca
saikrishnasangu <saikrishna.sangu1@gmail.com>
2023-05-08 14:02:49 +0000

    commit by admin1

--- react/react.sh
+++
@@ -1,9 +0,0 @@
-if [ -d "/home/coder/project/workspace/reactapp/src/tests/" ]
-    then
-        rm -r /home/coder/project/workspace/reactapp/src/tests;
-fi
-cp -r /home/coder/project/workspace/react/tests /home/coder/project/workspace/reactapp/src/;
-cd /home/coder/project/workspace/reactapp/;
-nvm use 14
-export CI=true;
-npx react-scripts test --verbose --testPathPattern=src/tests 2>&1;

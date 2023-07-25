5445124495b77de262e85046beb0d107d400fa07
KattachaithanyaKumar <20835a0504@gniindia.org>
2023-07-25 09:11:55 +0000

    app.js modified

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,5 +1,4 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
@@ -7,7 +6,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. chaithanya edited
         </p>
         <a
           className="App-link"

e7c199cff1e153a3350c838aa553a1ac1ab895da
abc <abc@example.com>
2023-07-29 23:05:49 +0530

    profile,profilecontroller_updated

--- springapp/src/main/java/com/examly/springapp/model/Profile.java
+++ springapp/src/main/java/com/examly/springapp/model/Profile.java
@@ -1,11 +1,12 @@
+//profile
 package com.examly.springapp.model;
-
-import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import lombok.*;
 
 @Builder
 @NoArgsConstructor

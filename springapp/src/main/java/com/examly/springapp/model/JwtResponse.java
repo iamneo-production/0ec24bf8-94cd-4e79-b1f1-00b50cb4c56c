20fedc64dbe4e921446a75d3c847e85dff2013c8
abc <abc@example.com>
2023-07-29 23:32:12 +0530

    commitedchanges

--- springapp/src/main/java/com/examly/springapp/model/JwtResponse.java
+++ springapp/src/main/java/com/examly/springapp/model/JwtResponse.java
@@ -1,27 +1,3 @@
-package com.examly.springapp.model;
-
 public class JwtResponse {
-    UserModel user ;
-    String jwtToken;
-
-    public UserModel getUser() {
-        return user;
-    }
-
-    public void setUser(UserModel user) {
-        this.user = user;
-    }
-
-    public String getJwtToken() {
-        return jwtToken;
-    }
-
-    public void setJwtToken(String jwtToken) {
-        this.jwtToken = jwtToken;
-    }
-
-    public JwtResponse(UserModel user, String jwtToken) {
-        this.user = user;
-        this.jwtToken = jwtToken;
-    }
+    
 }
--- springapp/src/main/java/com/examly/springapp/repository/ProfileRepo.java
+++ springapp/src/main/java/com/examly/springapp/repository/ProfileRepo.java
@@ -1,11 +1,3 @@
-package com.examly.springapp.repository;
-
-import com.examly.springapp.model.Profile;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProfileRepo extends JpaRepository<Profile,Long> {
+public class ProfileRepo {
     
 }

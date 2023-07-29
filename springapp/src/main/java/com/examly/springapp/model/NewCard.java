42a32d9c8c08e3ff69bf97cc12e578c949661522
SmrutisreePaik <piepaik@gmail.com>
2023-07-29 11:08:41 +0530

    docController and Model-NewCard added

--- springapp/src/main/java/com/examly/springapp/model/NewCard.java
+++ springapp/src/main/java/com/examly/springapp/model/NewCard.java
@@ -1,3 +1,46 @@
+package com.examly.springapp.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
 public class NewCard {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    String status = "in_progress" ;
+    String  areaName ;
+    String  birthDate;
+    String profileUrl ;
+    String emailId;
+    String fatherName;
+    String  firstName;
+    String gender;
+    String  houseNo;
+    String  lastName;
+    String  motherName;
+    String  nationality;
+    String peAreaName;
+    String peHouseNo;
+    String  peNationality;
+    String pePincode;
+    String peState;
+    String  peStreetName;
+    String  phoneNumber1;
+    String  phoneNumber2;
+    String  pincode;
+    String  state;
+    String streetName;
+    String aadhaarNumber ;
+
 }
